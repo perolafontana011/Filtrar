ebfb3faed8daeac9737c1a29f45f6225e0a11637
Pérola <perola.corchog@escola.pr.gov.br>
2026-03-17 08:44:06 -0300

    Update filtrar.js

--- filtrar.js
+++ filtrar.js
@@ -1,10 +1,12 @@
+// Cria variável que vai armazenar uma lista(array)
 const usuarios = [
     { nome: "Carlos", idade: 32 },
     { nome: "Ana", idade: 28 },
     { nome: "Felipe", idade: 40}
 ];
 
-const ana = usuarios.find(usuario => usuario.nome === "Ana");
+// O find vai buscar
+const ana = usuarios.find(usuario => usuario.nome === "Ana"); // === vai encontrar exatamente igual
 console.log(ana);
 
 const usuariosAcimaDe30 = usuarios.filter(usuario => usuario.idade > 30);
@@ -12,3 +14,6 @@ console.log(usuariosAcimaDe30);
 
 const usuariosOrdenadosPorIdade = usuarios.sort((a, b) => a.idade - b.idade);
 console.log(usuariosOrdenadosPorIdade);
+
+const usuariosOrdenadosPorIdade2 = usuarios.sort((a, b) => b.idade - a.idade);
+console.log(usuariosOrdenadosPorIdade2);
