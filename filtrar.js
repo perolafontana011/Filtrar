90d7b2aab363ea89c0ffabf5d0494d7d1d2d64f7
Pérola <perola.corchog@escola.pr.gov.br>
2026-03-17 08:14:18 -0300

    Update filtrar.js

--- filtrar.js
+++ filtrar.js
@@ -8,6 +8,7 @@ const ana = usuarios.find(usuario => usuario.nome === "Ana");
 console.log(ana);
 
 const usuariosAcimaDe30 = usuarios.filter(usuario => usuario.idade > 30);
+console.log(usuariosAcimaDe30);
 
 const usuariosOrdenadosPorIdade = usuarios.sort((a, b) => a.idade - b.idade);
 console.log(usuariosOrdenadosPorIdade);
